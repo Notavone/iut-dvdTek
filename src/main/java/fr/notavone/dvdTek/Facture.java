c7a16bb07b44ba2ea2afaa3864407664854f913a
Arthur dubois <arthur.dbs70@gmail.com>
2022-10-13 18:43:29 +0200

    Début de création de la facture

--- src/main/java/fr/notavone/dvdTek/Facture.java
+++ src/main/java/fr/notavone/dvdTek/Facture.java
@@ -1,6 +1,37 @@
 package fr.notavone.dvdTek;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Facture {
     private Location location;
     private double montant;
+    private Client client;
+    private ArrayList<Film> filmsChoisis;
+
+    public Facture(Client client, ArrayList<Film> filmsChoisis){
+        this.client = client;
+        this.filmsChoisis = filmsChoisis;
+    }
+
+    public Facture(Client client, Film filmChoisi){
+        this.client = client;
+        this.filmsChoisis = new ArrayList<>();
+        filmsChoisis.add(filmChoisi);
+    }
+
+    public double getMontant() {
+        return montant;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public ArrayList<Film> getFilmsChoisis() {
+        return filmsChoisis;
+    }
+
+
+
 }
