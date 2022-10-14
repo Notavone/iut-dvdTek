69b80d71977fad1639ae0d542cb3ea6c2445ab51
Arthur dubois <arthur.dbs70@gmail.com>
2022-10-14 18:39:07 +0200

    add fonctionnalités.txt, fix FactureTest

--- src/test/java/fr/notavone/dvdTek/FactureTest.java
+++ src/test/java/fr/notavone/dvdTek/FactureTest.java
@@ -5,6 +5,8 @@ import fr.notavone.dvdTek.Facture;
 public class FactureTest {
     private Facture facture;
     public FactureTest() {
-        facture = new Facture();
+        Film film = new Film("Le seigneur des anneaux", "Peter Jackson", "Fantastique", 2.56);
+        Client client = new Client("Leo", "hugonnot");
+        facture = new Facture(client, film);
     }
 }
